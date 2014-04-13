289b692a73d6a0f7fab1d23cfa8f79ce6c388819
Miguel Mínguez <m.minguezz@gmail.com>
2014-04-13 14:21:57 +0200

    Creando archivos de funciones

---
+++ kick-out/_kickout/theme-debug.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* kick-out functions for debug themes
+*
+*  Plugin : http://wordpress.org/plugins/debug-bar/
+*
+* @package kick-out
+*/
+
+// ppp ( $val ); -> Imprime $val legible para humanos sobre una variable 
+
+function ppp ( $val ) {
+	print ( '<pre>' )  ;
+	if ( is_array ( $val ) ) {
+		print_r ( $val ) ;
+	} else {
+		var_dump ( $val ) ;
+	}
+	print ( '</pre><br>' ) ;
+}
+
+?>
--- kick-out/functions.php
+++ kick-out/functions.php
@@ -15,4 +15,23 @@
 	add_theme_support( 'widgets' );
 	add_theme_support( 'post-thumbnails' );
 
+# GENERAL
+	include( TEMPLATEPATH . "/_kickout/theme-init.php" );  		// Confuguraciones de WordPress
+	include( TEMPLATEPATH . "/_kickout/theme-custmize.php" );   // Funciones personalizadas para el tema
+
+# DEBUG
+	if (WP_DEBUG){
+		include( TEMPLATEPATH . "/_kickout/theme-debug.php" );  // Funciones usadas durante la creaccion de temas completos
+	}
+
+# ADMIN
+	include( TEMPLATEPATH . "/_kickout/admin/dashboard-setup.php" );
+	include( TEMPLATEPATH . "/_kickout/admin/page-options.php" );
+
+# REGISTER
+	include( TEMPLATEPATH . "/_kickout/register/scripts.php" );
+	include( TEMPLATEPATH . "/_kickout/register/styles.php" );
+
+# PLUGINS
+	include( TEMPLATEPATH . "/_kickout/plugins/widget.php" );
 ?>
