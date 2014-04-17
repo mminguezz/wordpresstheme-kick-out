4be3500476a1ee05429731c211f8940e7f0248d0
Miguel Mínguez <m.minguezz@gmail.com>
2014-04-18 00:12:14 +0200

    completandocon wiki

--- kick-out/_kickout/theme-init.php
+++ kick-out/_kickout/theme-init.php
@@ -4,10 +4,12 @@
 
 add_action( 'after_setup_theme', 'kickout_theme_support' );
 
+
+
 function kickout_theme_support() {
 	add_theme_support( 'post-formats', array( 'aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat' ) );
-//	add_theme_support( 'post-thumbnails' );
-//	add_theme_support( 'custom-background' );
+	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'custom-background' );
 //	add_theme_support( 'custom-header' );
 //	add_theme_support( 'automatic-feed-links' );
 //	add_theme_support( 'menus' );
