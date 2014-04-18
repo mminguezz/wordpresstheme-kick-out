c0cd9aaa313d570e3cf341f43290d96753f03336
Miguel Mínguez <m.minguezz@gmail.com>
2014-04-18 23:36:27 +0200

    añadidas areas de menu y modificados contactos de usuarios

--- kick-out/_kickout/theme-init.php
+++ kick-out/_kickout/theme-init.php
@@ -3,8 +3,9 @@
 # THEME SUPPORT
 
 add_action( 'after_setup_theme', 'kickout_theme_support' );
+add_action( 'after_setup_theme', 'kickout_theme_menus' );
 
-
+add_filter('user_contactmethods','new_contactmethods',10,1);
 
 function kickout_theme_support() {
 	add_theme_support( 'post-formats', array( 'aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat' ) );
@@ -18,6 +19,22 @@ function kickout_theme_support() {
 	add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form') );
 }
 
+function kickout_theme_menus(){
+	register_nav_menus( array(
+		'primary' => __( 'Primary Menu', TEXT_DOMINE ),
+		'footer' => __( 'Footer Menu', TEXT_DOMINE )
+	) );
+}
+
+
+function kickout_contactmethods( $contactmethods ) {
+	$contactmethods['twitter'] = 'Twitter'; // Add Twitter
+	$contactmethods['facebook'] = 'Facebook'; // Add Facebook
+	unset($contactmethods['yim']); // Remove Yahoo IM
+	unset($contactmethods['aim']); // Remove AIM
+	unset($contactmethods['jabber']); // Remove Jabber
+	return $contactmethods;
+}
 
 
 ?>
