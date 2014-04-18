884695df97cc562fa438f2c90b62af176ad9f51a
Miguel Mínguez <m.minguezz@gmail.com>
2014-04-18 22:16:49 +0200

    añadido o eliminado theme_support

--- kick-out/_kickout/theme-init.php
+++ kick-out/_kickout/theme-init.php
@@ -9,11 +9,13 @@ add_action( 'after_setup_theme', 'kickout_theme_support' );
 function kickout_theme_support() {
 	add_theme_support( 'post-formats', array( 'aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat' ) );
 	add_theme_support( 'post-thumbnails' );
-	add_theme_support( 'custom-background' );
+//	add_theme_support( 'custom-background' );
+	remove_theme_support( 'custom-background' );
 //	add_theme_support( 'custom-header' );
+	remove_theme_support( 'custom-header' );
 //	add_theme_support( 'automatic-feed-links' );
-//	add_theme_support( 'menus' );
-//	add_theme_support( 'html5'  );
+	remove_theme_support( 'automatic-feed-links' );
+	add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form') );
 }
 
 
